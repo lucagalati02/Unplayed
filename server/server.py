087c0643743ee10c9bb3eefca09ebd98f2d16370
Luca Galati <lucagalati31@gmail.com>
2025-02-04 19:43:42 -0500

    setting up server to generate developer tokens

--- server/server.py
+++ server/server.py
@@ -1,19 +1,46 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 from datetime import datetime, timedelta, timezone
 import jwt
-from jwt import algorithms
-from jwt import PyJWKClient
-import time
-import requests
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.debug = True # Comment out this line for production
 CORS(app)
 
+# Apple Info
+load_dotenv()
+TEAM_ID = os.getenv('TEAM_ID')
+KEY_ID = os.getenv('KEY_ID')
+PRIVATE_KEY_PATH = os.getenv('PRIVATE_KEY_PATH')
+
 @app.route('/')
 def hello_unplayed():
     return jsonify({'message': 'Hello, Unplayed!'})
+
+@app.route('/get_developer_token', methods=['GET', 'POST'])
+def get_developer_token():
+    try:
+        with open(PRIVATE_KEY_PATH, 'r') as f:
+            private_key = f.read()
+
+        # Set token variables
+        headers = {
+            'alg': 'ES256',
+            'kid': KEY_ID
+        }
+        payload = {
+            'iss': TEAM_ID,
+            'iat': int (datetime.now(timezone.utc).timestamp()),
+            'exp': int((datetime.now(timezone.utc) + timedelta(days=180)).timestamp())
+        }
+
+        # Create the token
+        token = jwt.encode(payload, private_key, algorithm='ES256', headers=headers)
+        return jsonify({'token': token}), 200
+    except Exception as e:
+        return jsonify({'Error getting token': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
